bc5b4100a6dde98a2ea2dd5d244a73e239616560
Anuj Shah <Anuj.Shah@einfochips.com>
2022-06-07 15:49:32 +0530

    updated project with lombok

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -1,14 +1,31 @@
 package com.example.demo.entity;
 
-import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "event")
 public class Event {
  	@Id
@@ -48,100 +65,5 @@ public class Event {
     @OneToMany(targetEntity =  Category.class ,cascade = CascadeType. ALL, fetch=FetchType.LAZY)
     @JoinColumn(name= "fk_eventid",referencedColumnName = "Eventid")
     private Set<Category> category;
-    
-    //Order detail relationship
-    
-    
-	public Event(int eventid, String title, String description, String dateTime, Boolean status, String venue,
-			int totalSeats, Set<Category> category) {
-		super();
-		Eventid = eventid;
-		Title = title;
-		Description = description;
-		DateTime = dateTime;
-		Status = status;
-		Venue = venue;
-		TotalSeats = totalSeats;
-		this.category = category;
-	}
-	public Event() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Set<Category> getCategory() {
-		return category;
-	}
-	public void setCategory(Set<Category> category) {
-		this.category = category;
-	}
-	@Override
-	public String toString() {
-		return "Event [Eventid=" + Eventid + ", Title=" + Title + ", Description=" + Description + ", DateTime="
-				+ DateTime + ", Status=" + Status + ", Venue=" + Venue + ", TotalSeats=" + TotalSeats + ", category="
-				+ category + "]";
-	}
-	public int getEventid() {
-		return Eventid;
-	}
-
-
-
-	public void setEventid(int eventid) {
-		Eventid = eventid;
-	}
-
-
-
-	public String getTitle() {
-		return Title;
-	}
-
-
-
-	public void setTitle(String title) {
-		Title = title;
-	}
-
-
-
-	public String getDescription() {
-		return Description;
-	}
-	
-	public void setDescription(String description) {
-		Description = description;
-	}
-	
-	public String getDateTime() {
-		return DateTime;
-	}
-	
-	public void setDateTime(String dateTime) {
-		DateTime = dateTime;
-	}
-
-	public Boolean getStatus() {
-		return Status;
-	}
-
-	public void setStatus(Boolean status) {
-		Status = status;
-	}
-
-	public String getVenue() {
-		return Venue;
-	}
-
-	public void setVenue(String venue) {
-		Venue = venue;
-	}
-
-	public int getTotalSeats() {
-		return TotalSeats;
-	}
-	public void setTotalSeats(int totalSeats) {
-		TotalSeats = totalSeats;
-	}
-
 }
 
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -19,17 +19,17 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Transactional
 @Getter
 @Setter
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "user")
 public class User {
 		@Id
@@ -99,31 +99,4 @@ public class User {
 	 	@OneToMany(targetEntity =  OrderDetail.class ,cascade = CascadeType. ALL, fetch=FetchType.LAZY)
 	    @JoinColumn(name= "fk_userid",referencedColumnName = "username")
 	    private Set<OrderDetail> orderdetails;
-//	 	@OneToMany(mappedBy="user", cascade = CascadeType.ALL)
-//	 	Set<User> user;
-	 	
-	 	public User(String userName, String emailId, String userPassword, String name, @NotEmpty @Pattern(regexp = "(^$|[0-9]{10})", message = "invalid mobile number") @Size(min = 10, max = 12, message = "invalid mobile number") String mobileNo, String dateOfBirth,
-				String address, int pinCode, String state, String city, Set<Role> role, Set<OrderDetail> orderdetails) {
-				super();
-				this.userName = userName;
-				EmailId = emailId;
-				this.userPassword = userPassword;
-				Name = name;
-				MobileNo = mobileNo;
-				DateOfBirth = dateOfBirth;
-				Address = address;
-				PinCode = pinCode;
-				State = state;
-				City = city;
-				this.role = role;
-				this.orderdetails = orderdetails;
-			}
-			public User() {
-				// TODO Auto-generated constructor stub
-			}
-//			public User(String userName2, String emailId2, String userPassword2, String name2, String mobileNo2,
-//					String dateOfBirth2, String address2, int pinCode2, String state2, String city2, Role role2,
-//					OrderDetail orderDetail) {
-//				// TODO Auto-generated constructor stub
-//			}
 }
